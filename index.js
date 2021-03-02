e2fb3fe878aa6578b4e21663c1cea251dc724ecb
xujie <xujie@yixia.com>
2021-03-02 15:56:37 +0800

    .vue文件编译完成

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 const Koa = require('koa')
 const send = require('koa-send')
-const path =require('path')
+const path = require('path')
+const compilerSfc = require('@vue/compiler-sfc')
+const { Readable } = require('stream')
 
 const app = new Koa()
 
@@ -16,9 +18,17 @@ function streamToString(stream) {
     stream.on('error', reject)
   })
 }
+
+function stringToStream(text) {
+  const stream = new Readable()
+  stream.push(text)
+  stream.push(null) //结束要添加一个null
+  return stream
+}
 // app.use()开启中间件
 // 3.加载第三方模块
-app.use(async (ctx, next) => { //这里是处理静态文件之前，所以要放在服务之前
+// 这里是处理静态文件之前，所以要放在服务之前
+app.use(async (ctx, next) => { 
   // ctx.path --> /@modules/
   if(ctx.path.startsWith('/@modules/')){
     let moduleName = ctx.path.substr(10);
@@ -34,13 +44,40 @@ app.use(async (ctx, next) => {
   await send(ctx, ctx.path, {root: process.cwd(), index: 'index.html'})
   await next()
 })
+// 4.处理单文件组件，要在静态资源加载完后，处理第三个模块前，因为单文件组件内也有可能加载第三方组件
+app.use(async (ctx, next) => {
+  if(ctx.path.endsWith('.vue')){
+    const contents = await streamToString(ctx.body)
+    const { descriptor } = compilerSfc.parse(contents)
+    let code
+    if(!ctx.query.type){
+      code = descriptor.script.content
+      
+      code = code.replace(/export\s+default\s+/g, 'const __script = ')
+      code += `
+import { render as __render } from "${ctx.path}?type=template"
+__script.render = __render
+export default __script
+      `
+    }else if(ctx.query.type === 'template'){
+      const templateRender = compilerSfc.compileTemplate({ source: descriptor.template.content})
+      code = templateRender.code
+    }
+    ctx.type = 'application/javascript'
+    ctx.body = stringToStream(code)
+  }
+  await next()
+})
+
 // 2.修改第三方模块路劲
 app.use(async (ctx, next) => {
   if(ctx.type === 'application/javascript'){
     let contents = await streamToString(ctx.body)
     // import Vue from 'vue';  匹配此路径
     // import App from './App.vue'; 不匹配./ 因为可以访问到 正则?!不匹配的次正则
-    ctx.body = contents.replace(/(from\s+['"])(?!\.\/)/g, '$1/@modules/');
+    ctx.body = contents
+      .replace(/(from\s+['"])(?![\.\/])/g, '$1/@modules/')
+      .replace(/process\.env\.NODE_ENV/g, '"development"') //替换node中的环境变量
   }
   
   await next()
